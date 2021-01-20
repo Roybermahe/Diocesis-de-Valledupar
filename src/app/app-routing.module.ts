a72684303067c555136176108a1a7a1a74fdf698
Royber <roybermanjarrez@gmail.com>
2021-01-20 17:19:30 -0500

    create route index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', redirectTo: '/index', pathMatch: 'full'
+}, {
+  path: 'index', loadChildren: () => import('./index/index.module').then(m => m.IndexModule)
+}, {
+  path: '**', loadChildren: () => import('./index/index.module').then(m => m.IndexModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
